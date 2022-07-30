9d0711b5ff94df64684a7136d66290063225c993
monicacb28 <monicacb28@gmail.com>
2022-07-30 17:19:27 -0500

    version inicial proyecto

---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url: 'mongodb+srv://monicacb28:Monica0928@cluster0.cwagdj8.mongodb.net/mensajeriaapi?retryWrites=true&w=majority',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
