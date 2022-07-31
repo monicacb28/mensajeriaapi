28e773bffb10303d65db74d5f904d0463557bcad
monicacb28 <monicacb28@gmail.com>
2022-07-30 19:45:16 -0500

    entidades creadas

--- app/models/solicitud.model.js
+++ app/models/solicitud.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const SolicitudSchema = mongoose.Schema({
-    descripcion: {
+    idsolicitud : {
         type: String,
         index: true,
         unique: true,
@@ -9,21 +9,70 @@ const SolicitudSchema = mongoose.Schema({
         trim: true,
         minlength: 4
     },
+    idusuario: {
+        type: Number,
+        index: true,
+        unique: true,
+        required: true,
+        trim: true,
+        min: 5
+    },
+    descripcion: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
     tamano: {
         type: String,
         index: true,
-        unique: true,
         required: false,
         trim: true,
         minlength: 4
     },
-    price: {
-        type: Number,
-        min: 10
+    direccion: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
     },
-    expiration: Date
-}, {
-    timestamps: true    
+    ciudad: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
+    direcciondestino: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
+    ciudaddestino: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
+    nombredestinatario: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
+    estado: {
+        type: String,
+        index: true,
+        required: true,
+        trim: true,
+        minlength: 4
+    }
 });
 
 module.exports = mongoose.model('Solicitud', SolicitudSchema);
