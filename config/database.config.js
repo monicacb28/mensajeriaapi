cf612b48f398b06bea26126f5852df4270ff70fc
Christian Gil <christiancgil@gmail.com>
2022-07-30 21:18:27 -0500

    fixed untracked files

--- config/database.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    url: 'mongodb+srv://monicacb28:Monica0928@cluster0.cwagdj8.mongodb.net/mensajeriaapi?retryWrites=true&w=majority',
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-}
